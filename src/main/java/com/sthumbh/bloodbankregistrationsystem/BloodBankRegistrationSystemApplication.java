4ffbaa4f055f096f548a2cdbe90f08a33dc811ae
Mohammad Shahruk <mshahruk57@gmail.com>
2023-09-25 16:39:36 +0530

    initial commit

---
+++ src/main/java/com/sthumbh/bloodbankregistrationsystem/BloodBankRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodbankregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sthumbh/bloodbankregistrationsystem/BloodBankRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthumbh.bloodbankregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
